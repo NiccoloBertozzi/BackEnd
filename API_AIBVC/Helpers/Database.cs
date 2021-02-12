451135fd8202311af549202292c5da78b63803be
Riccardo <51778584+RiccardoMoretti-it@users.noreply.github.com>
2021-02-12 09:12:22 +0100

    Modificata api Iscrizioni
    ora mostra le iscrizioni solamente dei tornei non finiti

--- API_AIBVC/Helpers/Database.cs
+++ API_AIBVC/Helpers/Database.cs
@@ -647,7 +647,7 @@ namespace WebAPIAuthJWT.Helpers
             "LEFT JOIN Squadra ON ListaIscritti.IDSquadra = Squadra.IDSquadra)" +
             "LEFT JOIN Atleta atleta1 ON Squadra.IDAtleta1 = atleta1.IDAtleta)" +
             "LEFT JOIN Atleta atleta2 ON Squadra.IDAtleta2 = atleta2.IDAtleta)";
-            sql += "WHERE Squadra.IDAtleta1=@IDAtleta or Squadra.IDAtleta2=@IDAtleta";
+            sql += "WHERE(Squadra.IDAtleta1=@IDAtleta or Squadra.IDAtleta2=@IDAtleta) AND GETDATE()<CAST(Torneo.dataFine AS DATE)";
             comando = new SqlCommand(sql, conn);
             comando.Parameters.Add(new SqlParameter("IDAtleta", idAtleta));
             query = new DataTable();
